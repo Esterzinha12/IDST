6c0c176ef85903afa516d6023b130492f92c0b6c
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-05-08 20:25:14 -0300

    :+1:

---
+++ cypress/e2e/3-IDST/testesAta/testeIntegracao.cy.js
@@ -0,0 +1,8 @@
+describe('Teste de Integração', () => {
+    it('Verificar Ata', () => {
+        cy.request(GET, "localhost:8443/api/minute").as('MinuteRequest');
+        cy.get('@MinuteRequest').then((response) => {
+            expect(response.body).to.be.an('array');
+        })
+    })
+})
