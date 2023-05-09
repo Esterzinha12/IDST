067e58f3e7cb9b007dfba6bdc4120092e5e88858
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-05-09 14:40:43 -0300

    ok

--- cypress/e2e/3-IDST/testesLogin/testesCarga/testeCarga100x.cy.js
+++ cypress/e2e/3-IDST/testesLogin/testesCarga/testeCarga100x.cy.js
@@ -4,9 +4,11 @@ describe('Teste de Integração Login', () => {
             "corporateEmail": "vytor@weg.net",
             "workerPassword": "123"
         }
+        for(let i = 0; i < 100; i++){
         cy.request('POST', "localhost:8443/login/auth", user).as('LoginRequest');
         cy.get('@LoginRequest').then((response) => {
             expect(response.status).to.not.eq(500);
         })
+    }
     })
 })
--- cypress/e2e/3-IDST/testesProposta/testeProcesso.cy.js
+++ cypress/e2e/3-IDST/testesProposta/testeProcesso.cy.js
@@ -1,15 +1,18 @@
 describe('Teste de Processo Proposta', () => {
     it('Criar Proposta', () => {
+
+        cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
+
         const proposal = {
             "proposalCode": 1,
             "proposalName": "Nome da Proposta",
             "proposalStatus": "Backlog",
             "payback": 1.0,
-            "initialRunPeriod": 2023-01-01,
-            "finalExecutionPeriod": 2023-01-01,
+            "initialRunPeriod": "2023-01-01",
+            "finalExecutionPeriod": "2023-01-01",
             "descriptiveProposal": "Descritivo da Proposta",
             "responsibleAnalyst": {"workerCode": 2},
-            "demand": {"demandCode": 1},
+            "demand": {"demandCode": 1, "demandVersion": 1},
             "totalCosts": 2.0,
             "externalCosts": 1.0,
             "internalCosts": 1.0,
@@ -22,9 +25,9 @@ describe('Teste de Processo Proposta', () => {
             "published" : true
         }
 
-        cy.get('POST', "localhost:8443/api/proposal", proposal).as('ProposalRequest');
+        cy.request('POST', "localhost:8443/api/proposal", proposal).as('ProposalRequest');
         cy.get('@ProposalRequest').then((response) => {
-            expect(response.body).to.not(500);
+            expect(response.body).to.not.eq(500);
         })
     })
 
