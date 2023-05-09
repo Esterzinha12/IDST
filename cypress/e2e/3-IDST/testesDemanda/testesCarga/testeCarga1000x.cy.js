b4ce9b87e333634a8135b7e566efca63ec3805ef
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-05-08 21:44:37 -0300

    :+1:

--- cypress/e2e/3-IDST/testesDemanda/testesCarga/testeCarga1000x.cy.js
+++ cypress/e2e/3-IDST/testesDemanda/testesCarga/testeCarga1000x.cy.js
@@ -1,9 +1,9 @@
-describe('Teste de Integração', () => {
-    it('Verificar Ata', () => {
+describe('Teste de Carga 1000x Demanda', () => {
+    it('Verificar Demanda', () => {
         cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
         for(let i = 0; i < 1000; i++){
-        cy.request('GET', "localhost:8443/api/minutes").as('MinuteRequest');
-        cy.get('@MinuteRequest').then((response) => {
+        cy.request('GET', "localhost:8443/api/demand").as('DemandRequest');
+        cy.get('@DemandRequest').then((response) => {
             expect(response.body).to.be.an('array');
         })
     }
