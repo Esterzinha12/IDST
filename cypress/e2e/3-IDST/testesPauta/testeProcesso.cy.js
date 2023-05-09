365f57b115fcf508997df50ed4fb7baeb2e9d545
Eduarda Bolgenhagen <eduarda_b_campos@estudante.sesisenai.org.br>
2023-05-09 16:34:00 -0300

    :+1:

--- cypress/e2e/3-IDST/testesPauta/testeProcesso.cy.js
+++ cypress/e2e/3-IDST/testesPauta/testeProcesso.cy.js
@@ -1,4 +1,75 @@
 describe('Teste do processo Pauta', () => {
+
+
+    it('Criação Ata', () => {
+
+        cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
+
+        const minute = {
+            "minuteName":"Ata 1",
+            "minuteStartDate":"2023-05-09",
+            "minuteEndDate":"2023-05-30",
+            "minuteType":"1",
+            "agenda":{"agendaCode": 1},
+            "director":{"workerCode": 1}
+        }
+
+        cy.request('POST', "localhost:8443/api/minutes", minute).as('MinuteRequest');
+        cy.get('@MinuteRequest').then((response) => {
+            expect(response.status).to.not.eq(500)
+
+        })
+    })
+
+    it('Criação Comissão', () => {
+
+        cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
+
+        const commission = {
+            "commissionCode": 20,
+            "commissionName":"Comissão",
+            "commissionAcronym":"CL"
+        }
+
+        cy.request('POST', "localhost:8443/api/commission", commission).as('CommissionRequest');
+        cy.get('@CommissionRequest').then((response) => {
+            expect(response.status).to.not.eq(500)
+
+        })
+    })
+
+    it('Criar Proposta', () => {
+
+        cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
+
+        const proposal = {
+            "proposalCode": 1,
+            "proposalName": "Nome da Proposta",
+            "proposalStatus": "Backlog",
+            "payback": 1.0,
+            "initialRunPeriod": "2023-01-01",
+            "finalExecutionPeriod": "2023-01-01",
+            "descriptiveProposal": "Descritivo da Proposta",
+            "responsibleAnalyst": {"workerCode": 2},
+            "demand": {"demandCode": 1, "demandVersion": 1},
+            "totalCosts": 2.0,
+            "externalCosts": 1.0,
+            "internalCosts": 1.0,
+            "workers": [
+                {"workerCode": 1},
+                {"workerCode": 2}
+            ],
+            "proposalDate": "10/05/2023",
+            "commissionOpinion": "Opinião da comissão",
+            "published" : true
+        }
+
+        cy.request('POST', "localhost:8443/api/proposal", proposal).as('ProposalRequest');
+        cy.get('@ProposalRequest').then((response) => {
+            expect(response.body).to.not.eq(500);
+        })
+    })
+
     it('Criação Pauta', () => {
         cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
         const agenda = {
