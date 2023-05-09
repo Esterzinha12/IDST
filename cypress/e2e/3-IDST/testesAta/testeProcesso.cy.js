d54c1e4b43773571ecd69c50948afe93b105912d
Eduarda Bolgenhagen <eduarda_b_campos@estudante.sesisenai.org.br>
2023-05-09 16:37:22 -0300

    :+1:

--- cypress/e2e/3-IDST/testesAta/testeProcesso.cy.js
+++ cypress/e2e/3-IDST/testesAta/testeProcesso.cy.js
@@ -1,4 +1,40 @@
 describe('Teste do processo Ata', () => {
+   
+    it('Criação Pauta', () => {
+        cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
+        const agenda = {
+            "sequentialNumber": 2,
+            "yearAgenda": 2023,
+            "agendaDate": "2023-05-09",
+            "minutes": [{ "minuteCode": 2 }],
+            "commission": [{ "commissionCode": 2 }],
+            "proposals": [{ "proposalCode": 2 }]
+        }
+
+        cy.request('POST', "localhost:8443/api/agenda", agenda).as('AgendaRequest');
+        cy.get('@AgendaRequest').then((response) => {
+            expect(response.status).to.not.eq(500)
+
+        })
+    })
+
+    it('Criar Funcionário', () => {
+        cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
+        const worker = {
+            "workerCode": 5,
+            "workerName": "Funcionário",
+            "corporateEmail": "funcionario@weg.net",
+            "workerPassword": "123",
+            "workerOffice": "requester",
+            "language": "pt"
+        }
+
+        cy.request('POST', "localhost:8443/api/worker/1", worker).as('WorkerRequest');
+        cy.get('@WorkerRequest').then((response) => {
+            expect(response.status).to.not.eq(500);
+        })
+    })
+   
     it('Criação Ata', () => {
         cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
         const minute = {
