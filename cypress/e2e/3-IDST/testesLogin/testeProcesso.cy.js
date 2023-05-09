c9a09b79d77add0617c6008ad5afa919bcda585e
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-05-09 15:36:51 -0300

    ok

---
+++ cypress/e2e/3-IDST/testesLogin/testeProcesso.cy.js
@@ -0,0 +1,29 @@
+describe('Teste de Processo Login', () => {
+    it('Cadastrar Funcionario', () => {
+        cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
+        const user = {
+            "workerCode": "7334",
+            "workerName": "Alisson Ferraz",
+            "corporateEmail": "alisson@weg.net",
+            "workerPassword": "123",
+            "language": "pt"
+        }
+        cy.request('POST', "localhost:8443/api/worker/3", user).as('LoginRequest');
+        cy.get('@LoginRequest').then((response) => {
+            expect(response.status).to.not.eq(500);
+        })
+    })
+})
+
+describe('Teste de Processo Login', () => {
+    it('Verificar Login', () => {
+        const user = {
+            "corporateEmail": "alisson@weg.net",
+            "workerPassword": "123"
+        }
+        cy.request('POST', "localhost:8443/login/auth", user).as('LoginRequest');
+        cy.get('@LoginRequest').then((response) => {
+            expect(response.status).to.not.eq(500);
+        })
+    })
+})
