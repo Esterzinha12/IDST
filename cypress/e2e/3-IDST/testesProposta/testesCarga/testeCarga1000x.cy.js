3fb8f1dade116aeb8215d7a9afbbd1c1eaac3828
Eduarda Bolgenhagen <eduarda_b_campos@estudante.sesisenai.org.br>
2023-05-09 13:24:03 -0300

    :+1:

--- cypress/e2e/3-IDST/testesProposta/testesCarga/testeCarga1000x.cy.js
+++ cypress/e2e/3-IDST/testesProposta/testesCarga/testeCarga1000x.cy.js
@@ -1,7 +1,7 @@
 describe('Teste de Carga 1000x Proposta', () => {
     it('Verificar Proposta', () => {
-        for(let i = 0; i < 1000; i++){
         cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
+        for(let i = 0; i < 1000; i++){
         cy.request('GET', "localhost:8443/api/proposal").as('ProposalRequest');
         cy.get('@ProposalRequest').then((response) => {
             expect(response.body).to.be.an('array');
