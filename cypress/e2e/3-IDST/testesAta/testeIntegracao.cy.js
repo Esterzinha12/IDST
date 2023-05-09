66377fb677d29146a5a8c23d44e45d90a0ff2a6f
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-05-08 21:18:50 -0300

    ok

--- cypress/e2e/3-IDST/testesAta/testeIntegracao.cy.js
+++ cypress/e2e/3-IDST/testesAta/testeIntegracao.cy.js
@@ -1,6 +1,7 @@
 describe('Teste de Integração', () => {
     it('Verificar Ata', () => {
-        cy.request('GET', "localhost:8443/api/minute").as('MinuteRequest');
+        cy.setCookie("jwt", "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZHMiLCJzdWIiOiIyIiwiaWF0IjoxNjgzNTg5ODE1LCJleHAiOjE2ODM3Njk4MTV9.4Eu-pKnjlq5wIE-ZB5A52dCxxXvfvv45mKovfid5yZk");
+        cy.request('GET', "localhost:8443/api/minutes").as('MinuteRequest');
         cy.get('@MinuteRequest').then((response) => {
             expect(response.body).to.be.an('array');
         })
